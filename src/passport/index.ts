1c17451347e6f873d0808c6e700c117f7b67f060
QuqqU <rldnd913@naver.com>
2021-07-15 16:24:44 +0900

    add local strategy

---
+++ src/passport/index.ts
@@ -0,0 +1,6 @@
+// import passport from 'passport';
+import localStrategy from './localStrategy';
+
+export default function passportConfig(): void {
+    localStrategy();
+}
---
+++ src/passport/localStrategy.ts
@@ -0,0 +1,35 @@
+import passport from 'passport';
+import * as localPassport from 'passport-local';
+import bcrypt from 'bcrypt';
+
+import User from '../models/user';
+
+interface localStrategyConfigType {
+    usernameField: string,
+    passwordField: string
+}
+const localStrategyConfig: localStrategyConfigType = {
+    usernameField: 'id',
+    passwordField: 'password',
+};
+
+type doneFunction = (error?: Error, user?: Express.User, options?: localPassport.IVerifyOptions) => void;
+const localStrategyVerify: localPassport.VerifyFunction = async (id: string, password: string, done: doneFunction) => {
+    try {
+        const user: (User | null) = await User.findOne({ where: { id } });
+        if (!user) return done(undefined, undefined, { message: 'No Registered User' });
+
+        const isMatched: boolean = await bcrypt.compare(password, user.password);
+        if (!isMatched) return done(undefined, undefined, { message: 'Password does not match' });
+
+        return done(undefined, user);
+    }
+    catch (err) {
+        console.error(err);
+        return done(err);
+    }
+};
+
+export default function localStrategy(): void {
+    passport.use(new localPassport.Strategy(localStrategyConfig, localStrategyVerify));
+}

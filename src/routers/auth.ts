1db1cec7b92e693e04a85e8ce283981740899339
KU JUNG <rldnd913@naver.com>
2021-07-17 04:13:48 +0900

    update router method(prisma use)

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,53 +1,55 @@
-// import { Request, Response, NextFunction, Router } from 'express';
-// import bcrypt from 'bcrypt';
-// import passport from 'passport';
-// import User from '../db/models/user';
+import { Request, Response, NextFunction, Router } from 'express';
+import bcrypt from 'bcrypt';
+import passport from 'passport';
+import { PrismaClient, User } from '@prisma/client';
 
-// import { isLoggedIn, isNotLoggedIn } from '../middlewares/auth';
+import { isLoggedIn, isNotLoggedIn } from '../middlewares/auth';
 
-// const router: Router = Router();
+const router: Router = Router();
 
-// router.post('/signup', isNotLoggedIn, async (req: Request, res: Response, next: NextFunction) => {
-//     const { email, password }: { email: string; password: string } = req.body;
-//     // console.log(email, password);
-//     try {
-//         const user: (User | null) = await User.findOne({ where: { email } });
-//         if (user) return res.redirect('/signup?err=user_already_exist');
-//         const salt: number = Number(process.env.BCRYPT_SALT);
-//         const hashedPassword: string = await bcrypt.hash(password.toString(), salt);
-//         await User.create({
-//             email,
-//             password: hashedPassword,
-//         });
-//         return res.redirect('/');
-//     }
-//     catch (err) {
-//         console.error(err);
-//         return next(err);
-//     }
-// });
+router.post('/signup', isNotLoggedIn, async (req: Request, res: Response, next: NextFunction) => {
+    const prisma: PrismaClient = new PrismaClient();
+    const { email, password }: { email: string; password: string } = req.body;
 
-// interface Info {
-//     message: string
-// }
-// router.post('/login', isNotLoggedIn, (req: Request, res: Response, next: NextFunction) => {
-//     passport.authenticate('local', (authErr: Error, user: Express.User, info: Info) => {
-//         if (authErr) return next(authErr);
-//         if (!user) return res.redirect(`/login?loginErr=${info.message}`);
+    try {
+        const user: (User | null) = await prisma.user.findUnique({ where: { email } });
+        if (user) return res.redirect('/signup?err=user_already_exist');
+        const salt: number = Number(process.env.BCRYPT_SALT);
+        const hashedPassword: string = await bcrypt.hash(password.toString(), salt);
+        await prisma.user.create({
+            data: {
+                email,
+                password: hashedPassword,
+            },
+        });
+        return res.redirect('/');
+    }
+    catch (err) {
+        return next(err);
+    }
+});
 
-//         return req.login(user, (err: Error) => {
-//             if (err) {
-//                 console.error(err);
-//                 next(err);
-//             }
-//             return res.json({ });
-//         });
-//     })(req, res, next);
-// });
+interface Info {
+    message: string
+}
+router.post('/login', isNotLoggedIn, (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate('local', (authErr: Error, user: Express.User, info: Info) => {
+        if (authErr) return next(authErr);
+        if (!user) return res.redirect(`/login?loginErr=${info.message}`);
 
-// router.get('/logout', isLoggedIn, (req: Request, res: Response) => {
-//     // 대충 redis에서 뭐 없애는 코드
-//     res.redirect('/');
-// });
+        return req.login(user, (err: Error) => {
+            if (err) {
+                console.error(err);
+                next(err);
+            }
+            return res.json({ });
+        });
+    })(req, res, next);
+});
 
-// export default router;
+router.get('/logout', isLoggedIn, (req: Request, res: Response) => {
+    // 대충 redis에서 뭐 없애는 코드
+    res.redirect('/');
+});
+
+export default router;
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,25 +3,8 @@ import { Request, Response, NextFunction, Router } from 'express';
 const router: Router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    const a: number = 3;
-    console.log(a);
-    return res.send('hello typescript express!!');
-});
-
-import { PrismaClient, User } from '@prisma/client';
-
-router.get('/test', async (req: Request, res: Response, next: NextFunction) => {
-    const prisma: PrismaClient = new PrismaClient();
-    //
-
-    const nu: User = await prisma.user.create({
-        data: {
-            email: '123',
-            password: '2222',
-        },
-    });
-
-    return res.send(nu);
+    const resMessage: string = 'hello world';
+    return res.send(resMessage);
 });
 
 export default router;

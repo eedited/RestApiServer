b654783f8cdb34d49ff8ab1967bbbb4db1424e63
KU JUNG <rldnd913@naver.com>
2021-07-23 02:30:15 +0900

    update setting/rule/type

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,8 +1,8 @@
-// declare namespace Express {
-//     export interface Request {
-//         body: {
-//             email: string,
-//             password: string
-//         }
-//     }
-// }
+import prisma from '@prisma/client';
+
+declare global {
+    namespace Express {
+        // eslint-disable-next-line @typescript-eslint/no-empty-interface
+        export interface User extends prisma.User {}
+    }
+}

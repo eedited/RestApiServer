44ac5565f6c298ecdec5568bfc7a24db68f9c3e3
airplane9876 <tjdnf2eoeld@naver.com>
2021-07-27 14:08:26 +0900

    add auth on boilerplate

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,8 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
+import bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
+import DB from '../db';
 
 export const isLoggedIn: expressMiddleware = (req: Request, res: Response, next: NextFunction) => {
     if (req.isAuthenticated()) {
-        next();
+        return next();
     }
     return res.status(403).json({
         info: 'Login Required',
@@ -11,9 +14,36 @@ export const isLoggedIn: expressMiddleware = (req: Request, res: Response, next:
 
 export const isNotLoggedIn: expressMiddleware = (req: Request, res: Response, next: NextFunction) => {
     if (!req.isAuthenticated()) {
-        next();
+        return next();
     }
     return res.status(403).json({
         info: 'Account is already logged in',
     });
+};
+
+export const checkPassword: expressMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const { userId, password }: User = req.body;
+    try {
+        const user: (User | null) = await DB.prisma.user.findUnique({ where: { userId } });
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                info: 'Unregistered user',
+            });
+        }
+        const isPwCorrect: boolean = bcrypt.compareSync(password, user.password);
+        if (!isPwCorrect) {
+            return res.status(401).json({
+                success: false,
+                info: 'passwored inconsistency',
+            });
+        }
+    }
+    catch (err) {
+        return res.status(501).json({
+            success: false,
+        });
+    }
+
+    return next();
 };

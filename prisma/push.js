2d0e7747c01b5fb1f8ca09c7f96491a3db30e04a
QuqqU <rldnd913@naver.com>
2021-07-27 14:19:43 +0900

    v0.1.1 revise : 'prisma db push' error with env

---
+++ prisma/push.js
@@ -0,0 +1,20 @@
+const { exec } = require('child_process');
+require('dotenv').config();
+
+switch (process.env.NODE_ENV) {
+    case 'production':
+        process.env.DB_URL = process.env.DB_PRODUCTION_URL;
+        break;
+    case 'test':
+        process.env.DB_URL = process.env.DB_TEST_URL;
+        break;
+    default:
+        process.env.DB_URL = process.env.DB_DEVELOPMENT_URL;
+        break;
+}
+
+exec("npx prisma db push", (err, stdout, stderr) => {
+    err && console.log(err);
+    stdout && console.log(stdout);
+    stderr && console.log(stderr);
+})

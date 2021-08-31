5d9e7752278268e0150460fd5fe0bcfae512606c
airplane9876 <tjdnf2eoeld@naver.com>
2021-08-31 20:01:52 +0900

    ADD user/:userId router for mypage

--- src/routers/user.ts
+++ src/routers/user.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from 'express';
-import { User, Follower } from '@prisma/client';
+import { User, Video, VideoTag, Follower } from '@prisma/client';
+import { takeCoverage } from 'v8';
 import { isLoggedIn } from '../middlewares/auth';
 import DB from '../db';
 
@@ -62,9 +63,72 @@ router.patch('/:userId/follow', isLoggedIn, async (req: Request, res: Response) 
         }
         return res.status(200).json({});
     }
-    catch {
+    catch (err) {
         return res.status(500).json({
             info: `/user/${userId}/follow router error`,
+        });
+    }
+});
+
+router.get('/:userId', async (req: Request, res: Response) => {
+    const { userId }: typeof req.params = req.params;
+    try {
+        const user: (User & { Video: (Video & { WhoVideoUploadTag: { tagName: string; }[]; })[]; followFrom: { followingId: string; }[]; }) | null = await DB.prisma.user.findFirst({
+            where: {
+                userId,
+                deletedAt: null,
+            },
+            include: {
+                Video: {
+                    where: {
+                        uploader: userId,
+                        deletedAt: null,
+                    },
+                    include: {
+                        WhoVideoUploadTag: {
+                            where: {
+                                uploader: userId,
+                                deletedAt: null,
+                            },
+                            select: {
+                                tagName: true,
+                            },
+                        },
+                    },
+                },
+                followFrom: {
+                    where: {
+                        followerId: userId,
+                        deletedAt: null,
+                    },
+                    select: {
+                        followingId: true,
+                    },
+                },
+            },
+        });
+        if (!user) {
+            return res.status(404).json({
+                info: `/user/${userId} user not found`,
+            });
+        }
+        const tags: {[key: string]: number} = user.Video
+            .map((video: (Video & { WhoVideoUploadTag: { tagName: string; }[]; })) => video.WhoVideoUploadTag)
+            .reduce((acc: {tagName: string;}[], tag: {tagName: string;}[]) => [...acc, ...tag], [])
+            .map((tag: {tagName: string;}) => tag.tagName)
+            .reduce((acc: {[key: string]: number}, tag: string) => {
+                acc[tag] = (acc[tag] || 0) + 1;
+                acc.totalTagNum = (acc.totalTagNum || 0) + 1;
+                return acc;
+            }, {});
+        return res.status(200).json({
+            ...user,
+            tags,
+        });
+    }
+    catch (err) {
+        return res.status(500).json({
+            info: `/user/${userId} router error`,
         });
     }
 });

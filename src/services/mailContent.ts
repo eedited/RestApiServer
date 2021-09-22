ae35c68758cd44809d8a62afb886b539a9b8c8b4
JeonMinSu <minsu2530@u.sogang.ac.kr>
2021-09-23 02:04:53 +0900

    email을 사용할 때 accessToken이 만료되는 현상 없앰.

---
+++ src/services/mailContent.ts
@@ -0,0 +1,8 @@
+export const signupValidation: (src: string) => string = (src: string) => `
+        <div>
+            메일을 인증해주세요.
+            <div>안녕하세요 eedited입니다.</div>
+            <div>아래 메일 인증버튼을 눌러 회원가입을 완료해주세요</div>
+            <a href='http://127.0.0.1:4000/'>인증</a>
+        </div>
+    `;

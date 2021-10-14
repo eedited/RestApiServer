822e7d66da746cd06f83eebc7eff1246e4a9195c
JeonMinSu <minsu2530@u.sogang.ac.kr>
2021-10-14 14:29:58 +0900

    set cookie secure ture

--- src/app.ts
+++ src/app.ts
@@ -71,7 +71,7 @@ app.use(session({
     proxy: process.env.NODE_ENV === 'production',
     cookie: {
         httpOnly: true,
-        secure: false,
+        secure: true,
         maxAge: 1000 * 60 * 10,
     },
     store: sessionStoreOption,

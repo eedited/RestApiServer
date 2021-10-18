2ec614fbaf8ec61d494cffc4da4179623dded44c
JeonMinSu <minsu2530@u.sogang.ac.kr>
2021-10-18 15:46:45 +0900

    email validation url fix

--- src/services/mailContent.ts
+++ src/services/mailContent.ts
@@ -2,6 +2,6 @@ export const signupValidation: (token: string) => string = (token: string) => `
         <div>
             <div>안녕하세요 eedited입니다.</div>
             <div>아래 메일 인증버튼을 눌러 회원가입을 완료해주세요</div>
-            <a href='${process.env.PROTOCOL as string}${process.env.FE_URL as string}/emailValidation?token=${token}'>인증</a>
+            <a href='${process.env.FE_PROTOCOL as string}://${process.env.FE_URL as string}/emailValidation?token=${token}'>인증</a>
         </div>
     `;

9997dce0c6f4ddc7654dc8cb34520f899e470233
airplane9876 <tjdnf2eoeld@naver.com>
2021-10-20 00:47:50 +0900

    user테이블에 sns주소와 block여부 추가

---
+++ prisma/migrations/20211019153106_add_block_user/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `block` BOOLEAN NOT NULL DEFAULT false;
---
+++ prisma/migrations/20211019153922_add_sns_address/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `facebook` VARCHAR(150),
+    ADD COLUMN `instagram` VARCHAR(100),
+    ADD COLUMN `linkedin` VARCHAR(100);
